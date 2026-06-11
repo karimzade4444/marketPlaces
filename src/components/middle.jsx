76ec9d49cbe8e63c5a40a00dcbf56204db20240e
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-11 05:59:40 +0500

    middle

--- src/components/middle.jsx
+++ src/components/middle.jsx
@@ -1,9 +1,38 @@
-
+import { Button, Input } from "antd";
 
 const Middle = () => {
   return (
-    <div>Middle</div>
-  )
-}
+    <div className="w-[95%] m-auto">
+      <div className="h-45 bg-white rounded-xl border border-gray-300 p-7">
+        <div>
+          <p className="text-2xl font-bold">Добавить продукт</p>
+        </div>
+        <div className=" flex justify-between items-end pt-5">
+          <div>
+            <p>Название</p>
+            <Input
+              placeholder="Введите название..."
+              variant="filled"
+              className="w-100! mt-2!"
+            ></Input>
+          </div>
+          <div>
+            <p>Цена</p>
+            <Input
+              placeholder="0"
+              variant="filled"
+              className="w-100! mt-2!"
+            ></Input>
+          </div>
+          <div>
+            <Button type="primary" className="w-100! mt-2!">
+              + Добавить
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Middle
+export default Middle;
