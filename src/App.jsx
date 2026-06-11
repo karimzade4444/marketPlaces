8026e402ec7b8af701835cdc5a12cacd16b4e5be
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-11 05:45:24 +0500

    position

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Bottom from "./components/bottom";
 const App = () => {
   return (
     <div className=" relative">
-      <div className="w-full h-20 bg-white/90 border border-gray-300 flex justify-between items-center p-5 fixed">
+      <div className="w-full h-20 bg-white/90 border border-gray-300 flex justify-between items-center p-5 sticky top-0 z-10">
         <p className=" text-2xl font-semibold">Панель управление</p>
         <div className=" flex justify-center items-center gap-5">
           <Button >Гостевая Панель</Button>
