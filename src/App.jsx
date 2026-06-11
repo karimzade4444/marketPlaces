75e72a7d03ae774287fd3b40e8a6a55eed6f4c6d
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-11 05:40:49 +0500

    top

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-
+import { Button, Flex } from "antd";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className=" relative">
+      <div className="w-full h-20 bg-white/90 border border-gray-300 flex justify-between items-center p-5 fixed">
+        <p className=" text-2xl font-semibold">Панель управление</p>
+        <div className=" flex justify-center items-center gap-5">
+          <Button >Гостевая Панель</Button>
+          <Button type="primary">Админ Панель</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
